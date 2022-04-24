0fcc6c46574320be1a12bb24f60e3fd884c70dc0
Rifang <rifang.asmara12@gmail.com>
2022-04-24 21:34:40 +0700

    prepare again

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BasicCounter from "./pages/basic/Counter/classComponent";
 import BasicReact from "./pages/basic/React";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="basic/counter" element={<BasicCounter />} />
-        <Route path="basic/react" element= {<BasicReact />}/>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="basic/counter" element={<BasicCounter />} />
+//         <Route path="basic/react" element= {<BasicReact />}/>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
