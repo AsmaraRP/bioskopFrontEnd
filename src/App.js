206fcd3681af15bcda01eaeb7a4a9184a59f4b2c
Rifang <rifang.asmara12@gmail.com>
2022-06-28 15:06:25 +0700

    bug deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BasicCounter from "./pages/basic/Counter/classComponent";
-import BasicReact from "./pages/basic/React";
 import SignIn from "./pages/Login";
-import BasicLogin from "./pages/basic/Login";
-import BasicHome from "./pages/basic/Home";
-import BasicDetail from "./pages/basic/Detail";
-import BasicOrder from "./pages/basic/Order";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Order from "./pages/Order";
@@ -25,13 +19,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="basic/counter" element={<BasicCounter />} />
-        <Route path="basic/react" element={<BasicReact />} />
-        <Route path="basic/login" element={<BasicLogin />} />
-        <Route path="basic/home" element={<BasicHome />} />
-        <Route path="basic/order" element={<BasicOrder />} />
-        <Route path="basic/detail/:id" element={<BasicDetail />} />
-
         <Route path="login" element={<SignIn />} />
         <Route path="signup" element={<Signup />} />
         <Route path="home" element={<Home />} />
